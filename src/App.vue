8ec8df6466f8947074ab9fc6093bdb4912c9800a
Kuehar <green95251@gmail.com>
2020-03-07 12:40:10 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,120 @@
+<template>
+<div class="holder">
+    <div class="first"></div>
+    <div class="second"></div>
+    <div class="third"></div>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  @import url(https://fonts.googleapis.com/css?family=Bitter);
+    body {
+      margin: 0;
+      font-family: 'Bitter', serif;
+      text-align: center;
+    }
+    .holder,
+    .first,
+    .second,
+    .third {
+      height: 100vh;
+      width: 100vw;
+    }
+    .first,
+    .second,
+    .third {
+      position: absolute;
+    }
+    .second,
+    .third {
+      opacity: 0;
+    }
+    .holder {
+      height: 100vh;
+      position: relative;
+      z-index: 1;
+    }
+    .first {
+      animation: first 10s infinite;
+      background: linear-gradient(#ff7f50, #60e08c);
+      z-index: 10;
+    }
+      @keyframes first {
+        0% {opacity: 1.0;}
+        10% {opacity: 0.8;}
+        20% {opacity: 0.6;}
+        30% {opacity: 0.4;}
+        40% {opacity: 0.2;}
+        50% {opacity: 0.1;}
+        60% {opacity: 0.2;}
+        70% {opacity: 0.4;}
+        80% {opacity: 0.6;}
+        90% {opacity: 0.8;}
+        100% {opacity: 1.0;}
+      }
+
+    .second {
+      animation: second 10s infinite; animation-delay: 2s;
+      background: linear-gradient(#19eaa6, #00a1f0);
+      z-index: 20;
+    }
+      @keyframes second {
+        0% {opacity: 0;}
+        10% {opacity: 0.2;}
+        20% {opacity: 0.4;}
+        30% {opacity: 0.6;}
+        40% {opacity: 0.8;}
+        50% {opacity: 1.0;}
+        60% {opacity: 0.8;}
+        70% {opacity: 0.6;}
+        80% {opacity: 0.4;}
+        90% {opacity: 0.2;}
+        100% {opacity: 0;}
+      }
+
+    .third {
+      animation: third 10s infinite;
+      animation-delay: 8s;
+      background: linear-gradient(#aab7f8, #ffc44f);
+      z-index: 30;
+    }
+      @keyframes third {
+        0% {opacity: 0;}
+        10% {opacity: 0.2;}
+        20% {opacity: 0.4;}
+        30% {opacity: 0.6;}
+        40% {opacity: 0.8;}
+        50% {opacity: 1.0;}
+        60% {opacity: 0.8;}
+        70% {opacity: 0.6;}
+        80% {opacity: 0.4;}
+        90% {opacity: 0.2;}
+        100% {opacity: 0;}
+      }
+      #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        padding-top: 60px;
+        z-index:1000;
+        position: relative;
+        }
+      img{
+          border-radius:20%;
+          width:180px;
+          height:180px;
+          object-fit: cover;
+          }
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="hello">
+    <h1>"Welcome to Kuehar's page!!"</h1>
+    <h2>Links</h2>
+    <ul class="nav">
+      <li>
+        <a href="https://github.com/Kuehar"><ion-icon name="logo-github" class="ion-social-github"></ion-icon></a>
+      </li>
+      <li>
+          <a href="https://www.linkedin.com/in/uehara-ken-aa89b7152"><ion-icon name="logo-linkedin" class="ion-social-linkedin"></ion-icon></a>
+      </li>
+      <li>
+        <a href="https://twitter.com/KueharX"><ion-icon name="logo-twitter" class="ion-social-twitter"></ion-icon></a>
+      </li>
+      <li>
+        <a href="https://qiita.com/KueharX"><ion-icon name="reader-outline" class="ion-social-qiita"></ion-icon></a>
+      </li>
+      <br>
+    </ul>
+    <h2>This page was made using </h2>
+    <h2 class = "vue">Vue.js<a href="#"><ion-icon name="logo-vue" class="ion-social-vue"></ion-icon></a></h2>
+    <h2>and</h2>
+    <h2 class="azure">Microsoft Azure
+      <a href="#"><ion-icon name="logo-windows" class="ion-social-windows"></ion-icon></a></h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+h1, h2 {
+  font-weight: normal;
+  }
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: rgb(8, 8, 8);
+}
+.vue{
+  transition:3.0s;
+  color:#0A6F30;
+}
+.azure{
+  color:#00008B;
+}
+
+.ion-social-github,
+.ion-social-vue,
+.ion-social-linkedin,
+.ion-social-qiita,
+.ion-social-twitter,
+.ion-social-windows{
+  transition:color 0.2s;
+  color:#888;
+}
+
+.ion-social-github:hover{
+  color:#020303;
+}
+
+.ion-social-vue:hover{
+  color:#0A6F30;
+}
+
+.ion-social-linkedin:hover{
+  color:#00008B;
+}
+
+.ion-social-qiita:hover{
+  color:#0A6F30;
+}
+
+.ion-social-twitter:hover{
+  color:#55acee;
+}
+
+.ion-social-windows:hover{
+  color:#00008B;
+}
+
+.social-links{
+  list-style: none;
+  float: right;
+}
+
+.nav li a:link,
+.nav li a:visited,
+.li a:link,
+.li a:visited{
+  text-decoration: none;
+  border:0;
+  color:#888;
+  transition: 0.2s;
+}
+
+.li a:link,
+.li a:visited{
+  font-size:160%;
+}
+
+.page-container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
